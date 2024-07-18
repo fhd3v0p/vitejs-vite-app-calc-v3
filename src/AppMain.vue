15837532c1dfc8abfcfff7f4b02655452f98885e
fhd3v0p <fromhome@tuta.io>
2024-07-19 02:49:38 +0300

    v0.3.5

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -1,25 +1,31 @@
 <!-- src/AppMain.vue -->
 <template>
-  <div id="app-main">
+  <div id="app">
     <MainFon />
     <MainMenuButton />
     <MenuVes />
+    <MenuLitr />
+    <ResultPriceBox /> <!-- Добавляем новый компонент -->
   </div>
 </template>
 
 <script>
-import MainFon from './components/MainFon.vue'
-import MainMenuButton from './components/MainMenuButton.vue'
-import MenuVes from './components/MenuVes.vue'
+import MainFon from './components/MainFon.vue';
+import MainMenuButton from './components/MainMenuButton.vue';
+import MenuVes from './components/MenuVes.vue';
+import MenuLitr from './components/MenuLitr.vue';
+import ResultPriceBox from './components/ResultPriceBox.vue'; // Импортируем новый компонент
 
 export default {
   name: 'AppMain',
   components: {
     MainFon,
     MainMenuButton,
-    MenuVes
+    MenuVes,
+    MenuLitr,
+    ResultPriceBox  // Регистрируем новый компонент
   }
-}
+};
 </script>
 
 <style src="./styles.css"></style>

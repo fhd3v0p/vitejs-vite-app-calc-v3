bbacd07655be42117af15d0642addf9f5ad11fd5
fhd3v0p <fromhome@tuta.io>
2024-07-20 04:35:11 +0300

    v1.1.0

--- src/components/MenuLitr.vue
+++ src/components/MenuLitr.vue
@@ -1,37 +1,173 @@
-<!-- src/components/MenuLitr.vue -->
 <template>
-    <div class="menu-litr">
-      <svg
-        width="129"
-        height="234"
-        viewBox="0 0 129 234"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <circle cx="60" cy="114" r="60" fill="white" />
-        <path
-          d="M101.977 123H98.1847V113.04H95.4487L95.3767 114.6C95.2807 116.568 95.0967 118.104 94.8247 119.208C94.5527 120.296 94.1447 121.152 93.6007 121.776C93.0567 122.4 92.2647 122.968 91.2247 123.48L89.6407 121.08C90.0887 120.76 90.4407 120.408 90.6967 120.024C90.9527 119.624 91.1687 119.024 91.3447 118.224C91.5207 117.424 91.6647 116.264 91.7767 114.744L92.1127 110.256H101.977V123Z"
-          fill="#252525" />
-        <path
-          d="M37.5384 128V124.01H44.8044V103.85L37.8744 105.11V101.498L46.5264 98.6H49.2564V124.01H57.0684V128H37.5384ZM62.822 128V124.01H70.088V103.85L63.158 105.11V101.498L71.81 98.6H74.54V124.01H82.352V128H62.822Z"
-          fill="#252525" />
-        <circle cx="106.5" cy="22.5" r="22.5" fill="#F5513C" />
-        <path
-          d="M108.359 22.4844H112.648V25.4609H108.359V30.2891H105.254V25.4609H100.953V22.4844H105.254V17.8672H108.359V22.4844Z"
-          fill="#252525" />
-        <circle cx="106.5" cy="211.5" r="22.5" fill="#252525" />
-        <path
-          d="M112.359 212.789H102.598V210.152H112.359V212.789Z"
-          fill="#F5513C" />
-      </svg>
+  <div class="menu-litr">
+    <svg
+      width="129"
+      height="234"
+      viewBox="0 0 129 234"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <circle cx="60" cy="114" r="60" fill="white" />
+      <circle
+        cx="106.5"
+        cy="22.5"
+        r="22.5"
+        :class="['plus-litr-button', { 'active': isPlusActive }]"
+        @mousedown="isPlusActive = true"
+        @mouseup="isPlusActive = false"
+        @mouseleave="isPlusActive = false"
+        @click="incrementVolume"
+      />
+      <path
+        :class="['plus-litr-path', { 'active': isPlusActive }]"
+        d="M108.359 22.4844H112.648V25.4609H108.359V30.2891H105.254V25.4609H100.953V22.4844H105.254V17.8672H108.359V22.4844Z"
+      />
+      <circle
+        cx="106.5"
+        cy="211.5"
+        r="22.5"
+        :class="['minus-litr-button', { 'active': isMinusActive }]"
+        @mousedown="isMinusActive = true"
+        @mouseup="isMinusActive = false"
+        @mouseleave="isMinusActive = false"
+        @click="decrementVolume"
+      />
+      <path
+        :class="['minus-litr-path', { 'active': isMinusActive }]"
+        d="M112.359 212.789H102.598V210.152H112.359V212.789Z"
+      />
+    </svg>
+    <div class="volume-display">
+      <div class="volume-number-unit">
+        <div class="volume-number">{{ volume }}</div>
+        <div class="volume-unit">
+          <div>л</div>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .menu-litr {
-    position: absolute;
-    top: 52px; /* Отступ сверху */
-    right: 21px; /* Отступ справа */
-    z-index: 2;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuLitr',
+  data() {
+    return {
+      volume: 0, // Начальное значение объема
+      volumes: [0, 0.5, 1], // Возможные значения объема
+      currentIndex: 0, // Текущий индекс в массиве volumes
+      isPlusActive: false, // Состояние для кнопки плюс
+      isMinusActive: false, // Состояние для кнопки минус
+    };
+  },
+  methods: {
+    incrementVolume() {
+      if (this.currentIndex < this.volumes.length - 1) {
+        this.currentIndex++;
+      } else {
+        this.volumes.push(this.volumes[this.volumes.length - 1] + 1);
+        this.currentIndex++;
+      }
+      this.volume = this.volumes[this.currentIndex];
+    },
+    decrementVolume() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--;
+        this.volume = this.volumes[this.currentIndex];
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.menu-litr {
+  position: absolute;
+  top: 52px; /* Отступ сверху */
+  right: 21px; /* Отступ справа */
+  z-index: 2;
+}
+
+.volume-display {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-family: 'DM Mono', monospace;
+  font-weight: medium;
+  color: #252525;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.volume-number-unit {
+  display: flex;
+  align-items: flex-end;
+}
+
+.volume-number {
+  font-size: 42px;
+  line-height: 1;
+  letter-spacing: 0.1px;
+}
+
+.volume-unit {
+  font-size: 24px; /* Размер для лучшей читаемости */
+  line-height: 1;
+  letter-spacing: 0.1px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-left: 8px; /* Отступ от числа */
+  font-weight: bold; /* Сделаем текст более жирным */
+}
+
+/* Стили для кнопок и эффект нажатия */
+.plus-litr-button,
+.minus-litr-button {
+  cursor: pointer;
+  transition: fill 0.2s, stroke 0.2s;
+}
+
+.plus-litr-path,
+.minus-litr-path {
+  transition: fill 0.2s;
+}
+
+.plus-litr-button.active {
+  fill: white;
+}
+
+.plus-litr-path.active {
+  fill: #F5513C;
+}
+
+.minus-litr-button.active {
+  fill: white;
+}
+
+.minus-litr-path.active {
+  fill: #252525;
+}
+
+.plus-litr-button {
+  fill: #F5513C;
+  transition: fill 0.2s;
+}
+
+.plus-litr-path {
+  fill: #252525;
+  transition: fill 0.2s;
+}
+
+.minus-litr-button {
+  fill: #252525;
+  transition: fill 0.2s;
+}
+
+.minus-litr-path {
+  fill: #F5513C;
+  transition: fill 0.2s;
+}
+</style>

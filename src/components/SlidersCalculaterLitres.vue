474918404caa86a617dbdc76a76d0e8385875f18
fhd3v0p <fromhome@tuta.io>
2024-07-20 06:04:06 +0300

    v1.1.2(quasar)

--- src/components/SlidersCalculaterLitres.vue
+++ src/components/SlidersCalculaterLitres.vue
@@ -1,51 +1,98 @@
 <template>
-    <svg
-      width="151"
-      height="160"
-      viewBox="0 0 151 160"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M14 157L14 52"
-        stroke="#EA3C53"
-        stroke-opacity="0.5"
-        stroke-width="5"
-        stroke-linecap="round"
-        stroke-dasharray="10 10" />
-      <path
-        d="M14 65.3333C21.3638 65.3333 27.3334 59.3638 27.3334 52C27.3334 44.6362 21.3638 38.6667 14 38.6667C6.63623 38.6667 0.666698 44.6362 0.666698 52C0.666698 59.3638 6.63623 65.3333 14 65.3333ZM16.5 14C16.5 12.6193 15.3807 11.5 14 11.5C12.6193 11.5 11.5 12.6193 11.5 14H16.5ZM16.5 52L16.5 14H11.5L11.5 52H16.5Z"
-        fill="#EA3C53" />
-      <path
-        d="M148 14L14 14"
-        stroke="#FFBD9C"
-        stroke-opacity="0.5"
-        stroke-width="5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-dasharray="10 10" />
-      <path
-        d="M57.3334 14C57.3334 6.6362 51.3638 0.666667 44 0.666667C36.6362 0.666667 30.6667 6.6362 30.6667 14C30.6667 21.3638 36.6362 27.3333 44 27.3333C51.3638 27.3333 57.3334 21.3638 57.3334 14ZM14 11.5C12.6193 11.5 11.5 12.6193 11.5 14C11.5 15.3807 12.6193 16.5 14 16.5L14 11.5ZM44 11.5L14 11.5L14 16.5L44 16.5V11.5Z"
-        fill="#FF7B39" />
-      <path
-        d="M47.0438 49.7974C52.4547 44.8027 52.7921 36.3672 47.7974 30.9563C42.8027 25.5453 34.3672 25.2079 28.9563 30.2026C23.5454 35.1973 23.2079 43.6328 28.2027 49.0437C33.1974 54.4547 41.6328 54.7921 47.0438 49.7974ZM15.837 12.3043C14.9005 11.2897 13.3189 11.2265 12.3043 12.163C11.2897 13.0995 11.2265 14.6811 12.163 15.6957L15.837 12.3043ZM39.837 38.3043L15.837 12.3043L12.163 15.6957L36.163 41.6957L39.837 38.3043Z"
-        fill="#F5523D" />
-      <path
-        d="M148 157L39 40"
-        stroke="#F5523D"
-        stroke-opacity="0.5"
-        stroke-width="5"
-        stroke-linecap="round"
-        stroke-dasharray="10 10" />
-    </svg>
-  </template>
-  
-  <script>
-  export default {
-    name: 'SlidersCalculaterLitres',
+  <div class="sliders-container">
+    <q-slider
+      v-model="length"
+      :min="0"
+      :max="50"
+      color="#EA3C53"
+      track-color="rgba(234, 60, 83, 0.5)"
+      thumb-color="#EA3C53"
+      label
+      label-always
+      vertical
+      class="length-slider"
+    />
+    <q-slider
+      v-model="width"
+      :min="0"
+      :max="50"
+      color="#FFBD9C"
+      track-color="rgba(255, 189, 156, 0.5)"
+      thumb-color="#FFBD9C"
+      label
+      label-always
+      class="width-slider"
+    />
+    <q-slider
+      v-model="height"
+      :min="0"
+      :max="50"
+      color="#F5523D"
+      track-color="rgba(245, 82, 61, 0.5)"
+      thumb-color="#F5523D"
+      label
+      label-always
+      class="height-slider"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'SlidersCalculaterLitres',
+  setup() {
+    const length = ref(0);
+    const width = ref(0);
+    const height = ref(0);
+
+    return {
+      length,
+      width,
+      height
+    }
   }
-  </script>
-  
-  <style scoped>
-  /* Дополнительные стили можно добавить здесь */
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.sliders-container {
+  position: relative;
+  width: 151px;
+  height: 160px;
+  display: flex;
+  justify-content: space-between;
+  z-index: 11; /* Устанавливаем высокий z-index для контейнера */
+}
+
+.length-slider {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin-top: 52px;
+  margin-bottom: 3px;
+  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+}
+
+.width-slider {
+  position: absolute;
+  left: 14px;
+  right: 0;
+  margin-left: 14px;
+  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+}
+
+.height-slider {
+  position: absolute;
+  left: 14px;
+  top: 0;
+  bottom: 0;
+  margin-left: 25px;
+  margin-top: 52px;
+  margin-bottom: 3px;
+  transform: rotate(45deg);
+  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+}
+</style>

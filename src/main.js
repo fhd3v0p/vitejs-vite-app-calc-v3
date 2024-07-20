693f44d76b6d265f4e85dee4a33b495d4538ec8e
fhd3v0p <fromhome@tuta.io>
2024-07-21 01:36:55 +0300

    v1.2.1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // src/main.js
 import { createApp } from 'vue';
+import { Quasar } from 'quasar';
 import AppMain from './AppMain.vue';
+import quasarUserOptions from './quasar-user-options';
 import './styles.css';
 
-createApp(AppMain).mount('#app');
+createApp(AppMain)
+  .use(Quasar, quasarUserOptions)
+  .mount('#app');
---
+++ src/quasar-user-options.js
@@ -0,0 +1,10 @@
+// src/quasar-user-options.js
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+
+export default {
+  config: {},
+  plugins: {
+    // Включите необходимые плагины Quasar здесь
+  }
+};

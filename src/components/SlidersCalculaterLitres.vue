d01803c08596b49e8c2e673fae56f79e1153bd5c
fhd3v0p <fromhome@tuta.io>
2024-07-20 06:05:57 +0300

    v1.1.4

--- src/components/SlidersCalculaterLitres.vue
+++ src/components/SlidersCalculaterLitres.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="sliders-container">
-    <q-slider
-      v-model="length"
-      :min="0"
-      :max="50"
-      color="#EA3C53"
-      track-color="rgba(234, 60, 83, 0.5)"
-      thumb-color="#EA3C53"
-      label
-      label-always
-      vertical
-      class="length-slider"
-    />
-    <q-slider
-      v-model="width"
-      :min="0"
-      :max="50"
-      color="#FFBD9C"
-      track-color="rgba(255, 189, 156, 0.5)"
-      thumb-color="#FFBD9C"
-      label
-      label-always
-      class="width-slider"
-    />
-    <q-slider
-      v-model="height"
-      :min="0"
-      :max="50"
-      color="#F5523D"
-      track-color="rgba(245, 82, 61, 0.5)"
-      thumb-color="#F5523D"
-      label
-      label-always
-      class="height-slider"
-    />
+    <div class="slider-wrapper length-slider">
+      <input
+        type="range"
+        v-model="length"
+        min="0"
+        max="50"
+        class="slider"
+        @input="updateSlider('length', $event)"
+      />
+      <div class="slider-label">{{ length }}</div>
+    </div>
+    <div class="slider-wrapper width-slider">
+      <input
+        type="range"
+        v-model="width"
+        min="0"
+        max="50"
+        class="slider"
+        @input="updateSlider('width', $event)"
+      />
+      <div class="slider-label">{{ width }}</div>
+    </div>
+    <div class="slider-wrapper height-slider">
+      <input
+        type="range"
+        v-model="height"
+        min="0"
+        max="50"
+        class="slider"
+        @input="updateSlider('height', $event)"
+        style="transform: rotate(45deg);"
+      />
+      <div class="slider-label">{{ height }}</div>
+    </div>
   </div>
 </template>
 
@@ -47,11 +47,26 @@ export default {
     const width = ref(0);
     const height = ref(0);
 
+    const updateSlider = (type, event) => {
+      switch (type) {
+        case 'length':
+          length.value = event.target.value;
+          break;
+        case 'width':
+          width.value = event.target.value;
+          break;
+        case 'height':
+          height.value = event.target.value;
+          break;
+      }
+    };
+
     return {
       length,
       width,
-      height
-    }
+      height,
+      updateSlider
+    };
   }
 }
 </script>
@@ -66,33 +81,58 @@ export default {
   z-index: 11; /* Устанавливаем высокий z-index для контейнера */
 }
 
-.length-slider {
-  position: absolute;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  margin-top: 52px;
-  margin-bottom: 3px;
-  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+.slider-wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.width-slider {
-  position: absolute;
-  left: 14px;
-  right: 0;
-  margin-left: 14px;
-  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+.length-slider .slider {
+  width: 8px;
+  height: 100px;
+  background: rgba(234, 60, 83, 0.5);
+  -webkit-appearance: none;
+  appearance: none;
 }
 
-.height-slider {
-  position: absolute;
-  left: 14px;
-  top: 0;
-  bottom: 0;
-  margin-left: 25px;
-  margin-top: 52px;
-  margin-bottom: 3px;
-  transform: rotate(45deg);
-  z-index: 12; /* Устанавливаем высокий z-index для видимости */
+.width-slider .slider {
+  width: 100px;
+  height: 8px;
+  background: rgba(255, 189, 156, 0.5);
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.height-slider .slider {
+  width: 8px;
+  height: 100px;
+  background: rgba(245, 82, 61, 0.5);
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.slider::-webkit-slider-thumb {
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #EA3C53; /* Change color for each slider using CSS classes */
+  cursor: pointer;
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.width-slider .slider::-webkit-slider-thumb {
+  background: #FFBD9C;
+}
+
+.height-slider .slider::-webkit-slider-thumb {
+  background: #F5523D;
+}
+
+.slider-label {
+  margin-top: 8px;
+  font-size: 14px;
+  color: #252525;
 }
 </style>
